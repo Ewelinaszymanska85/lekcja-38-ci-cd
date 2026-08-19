ad4987aa02dfc08b638e361182a0de18e5ada3e8
Ewelinaszymanska85 <27kwiecien1985@gmail.com>
2026-08-19 11:22:02 +0200

    Add first pytest workflow

---
+++ tests/test_math.py
@@ -0,0 +1,9 @@
+"""
+Zadanie 4 - Pierwszy test.
+Prosty test jednostkowy sprawdzający poprawność dodawania.
+"""
+
+
+def test_addition():
+    """Sprawdza, czy wynik działania 2 + 2 jest równy 4."""
+    assert 2 + 2 == 4
