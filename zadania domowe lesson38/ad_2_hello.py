976f54f6c7b31d4326a160121fa6ae9663b3404b
Ewelinaszymanska85 <27kwiecien1985@gmail.com>
2026-08-19 11:05:01 +0200

    Add task 2 Python workflow

---
+++ zadania domowe lesson38/ad_2_hello.py
@@ -0,0 +1,14 @@
+"""
+Workflow z Pythonem.
+Prosty program uruchamiany przez GitHub Actions
+w środowisku Python 3.11.
+"""
+
+
+def hello():
+    """Wyświetla komunikat potwierdzający poprawne działanie programu."""
+    print("Hello from Python 3.11!")
+
+
+if __name__ == "__main__":
+    hello() 
